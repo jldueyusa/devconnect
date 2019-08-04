a41b37359fb4fea49ae043921cb3d4086930b9ee
Jennifer Duey <jlduey@gmail.com>
2019-08-04 17:07:10 -0400

    created react project

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
