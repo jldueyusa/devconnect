8b38a636b015d495e7b275bdf2a24e39c01472c4
Jennifer Duey <jlduey@gmail.com>
2019-08-04 14:00:27 -0400

    adding post information

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // connect database
 connectDB();
 
+
+// init midleware
+app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
